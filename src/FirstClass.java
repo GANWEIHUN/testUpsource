3e77a8190326fa0a94858291b09833dd4fc67e72
ganwh <ganweihun@yunksj.com>
2020-09-02 18:04:57 +0800

    第一次提交

---
+++ src/FirstClass.java
@@ -0,0 +1,30 @@
+/**
+ * @author: ganwh
+ * @since: 2020/09/02 18:00
+ * @Description:
+ */
+public class FirstClass {
+
+    //region fields
+
+    //endregion
+
+    //region public methods
+
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+    //endregion
+
+    //region constructor
+
+    //endregion
+
+    //region private methods
+
+    //endregion
+
+    //region overrides
+
+    //endregion
+}
