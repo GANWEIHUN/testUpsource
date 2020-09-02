0655ec565a6eecbc4de2c8427e06e1c64c2f8997
ganwh <ganweihun@yunksj.com>
2020-09-02 19:33:56 +0800

    第二次提交

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -13,6 +13,7 @@ public class FirstClass {
 
     public static void main(String[] args) {
         System.out.println("hello world!");
+        System.out.println("I am coming!");
     }
     //endregion
 
